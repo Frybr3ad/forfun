98f196e9e488b5f2e03379c3599b34ea116dec85
Angelina Jimenez <angelinajimenez425@gmail.com>
2023-05-02 17:42:12 -0700

    dogs

---
+++ 4-27/js/script.js
@@ -0,0 +1,9 @@
+var firstName = "Angelina";
+var lastName = "Shatzka";
+var age = 33;
+var favAnimal = "black lab";
+var leastFav = "CATS! EEWW!";
+console.log(` My name is ${firstName} ${lastName}. I'm ${age} years old. My favorite animal is a ${favAnimal}. 
+I love them so much! they're smart. loyal, good family dogs, super sweet but great 
+guard dogs too. My least favorite animal is ${leastFav}! Well mine, shes so nasty 
+I have never in my life seen a cat do the disgusting things she does..`);
